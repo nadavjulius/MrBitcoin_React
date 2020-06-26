ebfd97c63b8eadfeb5e93dfd49fd3796251563cd
Nadav Julius <nadavjulius@gmail.com>
2020-06-27 01:32:03 +0300

    Updates

---
+++ precache-manifest.a4ef1102ca54de459de015d3d7e1e861.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce042ba13a873b6b662bd47b326b0ce2",
+    "url": "/MrBitcoin_React/index.html"
+  },
+  {
+    "revision": "c3a5f8d7175198b9e751",
+    "url": "/MrBitcoin_React/static/css/2.cd751d61.chunk.css"
+  },
+  {
+    "revision": "ecc619cb4733e5e0d860",
+    "url": "/MrBitcoin_React/static/css/main.464b4139.chunk.css"
+  },
+  {
+    "revision": "c3a5f8d7175198b9e751",
+    "url": "/MrBitcoin_React/static/js/2.b64bbf30.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/MrBitcoin_React/static/js/2.b64bbf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc619cb4733e5e0d860",
+    "url": "/MrBitcoin_React/static/js/main.c3467861.chunk.js"
+  },
+  {
+    "revision": "ba749427bedb8e7cd580",
+    "url": "/MrBitcoin_React/static/js/runtime-main.6743fa03.js"
+  },
+  {
+    "revision": "03e45fc538903e65744cb13032b9b426",
+    "url": "/MrBitcoin_React/static/media/growth-chart.03e45fc5.png"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/MrBitcoin_React/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "febe1ea74cdee1cf2c2d5abde72e9829",
+    "url": "/MrBitcoin_React/static/media/petertheil.febe1ea7.JPG"
+  },
+  {
+    "revision": "224247e6f3a2e8be88d1b9d87396a380",
+    "url": "/MrBitcoin_React/static/media/unknown.224247e6.png"
+  },
+  {
+    "revision": "77192ef05398bb12cf806695f90fca8e",
+    "url": "/MrBitcoin_React/static/media/wallet.77192ef0.png"
+  }
+]);
